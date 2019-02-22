4a1f57dd2009d97cd512b77441b3d0ed99d49133
Manolis Kalafatis <mankal111@hotmail.com>
2019-02-22 11:19:37 +0200

    changed default nwb files

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,21 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { Exercise } from "../../src";
 
-import Example from '../../src'
-
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-exercise-component Demo</h1>
-      <Example/>
-    </div>
-  }
+export default class extends Component {
+    render() {
+        return (
+            <div style={{ width: 640, margin: "15px auto" }}>
+                <h1>Exercise component demo</h1>
+                <Exercise
+                    title="test exercise"
+                    description="this is a test exercise"
+                    question={["\\text{Can you type $1$?}"]}
+                    answerFields={[{ type: 'text-input', id: 'answer' }]}
+                    checkAnswer={(a) => a.answer === '1' ? {isCorrect: true} : {isCorrect: false}}
+                    generateNewValues={() => {}}
+                    answerComment={[]}
+                />
+            </div>
+        );
+    }
 }
-
-render(<Demo/>, document.querySelector('#demo'))
---
+++ src/Exercise.js
@@ -0,0 +1,135 @@
+import React from "react"
+import {Paper, Button, withStyles} from "@material-ui/core"
+import 'katex/dist/katex.min.css'
+import {InlineMath} from "react-katex"
+//import Solution from "./Solution";
+
+const styles = {
+    root: {
+        maxWidth: 400,
+        padding: 18
+    },
+    title: {
+        margin: 0
+    }
+}
+
+export class Exercise extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {solutionVisible: false};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        //this.toggleSolution = this.toggleSolution.bind(this);
+        this.newExercise = this.newExercise.bind(this);
+    }
+
+    handleChange(event) {
+        const answerFields = {
+            ...this.state.answerFields,
+            [event.target.name]: event.target.value
+        }
+        this.setState({ answerFields });
+    }
+
+    handleSubmit(event) {
+        const checkAnswerObject = this.props.checkAnswer(this.state.answerFields);
+        if (checkAnswerObject.isCorrect) {
+            if (window.confirm("Correct!\nDo you want to try this exercise with new values?")) {
+                this.newExercise();
+            };
+        } else {
+            alert(checkAnswerObject.message);
+        }
+        event.preventDefault();
+    }
+
+    // toggleSolution() {
+    //     this.setState({ solutionVisible: !this.state.solutionVisible })
+    // }
+
+    newExercise() {
+        this.props.generateNewValues();
+        this.resetInputFields();
+        this.setState({ solutionVisible: false });
+    }
+
+    inputElements() {
+        return this.props.answerFields.map((answerComponent, i) => {
+            if (answerComponent.type === "text-input") {
+                return <input
+                    type="text"
+                    value={this.state.answerFields[answerComponent.id]}
+                    onChange={this.handleChange}
+                    className="decimal"
+                    name={answerComponent.id}
+                    key={i}
+                />
+            } else if (answerComponent.type === "select-correct") {
+                return typeof answerComponent.correct !== 'undefined' ? <select 
+                    key={i}
+                    onChange={this.handleChange}
+                    name={`${i}-selected-item`}
+                    value={this.state[`${i}-selected-item`]}
+                >
+                    {answerComponent.items.map((item, i) => <option key={i} value={i}>{item}</option>)}
+                </select> : null;
+            } else if (answerComponent.type === "text") {
+                // return <InlineMath key={i} math={answerComponent.content} />
+                return <div>{answerComponent.content}</div>
+            }
+        })
+    }
+
+    resetInputFields() {
+        const answerFields = { ...this.state.answerFields };
+        this.props.answerFields.forEach((answerComponent, i) => {
+            if (answerComponent.type === "text-input") {
+                answerFields[answerComponent.id] = '';
+            } else if (answerComponent.type === "select-correct") {
+                answerFields[answerComponent.id] = 0;
+            }
+        })
+        this.setState({answerFields})
+    }
+
+    componentWillMount() {
+        this.resetInputFields();
+    }
+
+    render() {
+        const { title, description, question, answerComment, classes } = this.props;
+        return <Paper className={classes.root}>
+            <h2 className={classes.title}>{title}</h2>
+            <div>{description}</div>
+            <div>
+                {question.map(item => <InlineMath math={'\\text{' + item + '}'} />)}
+            </div>
+            <div>
+                {answerComment.map((item, i) => <div key={i}>{item}</div>)}
+            </div>
+            <div className="answer-section">
+                {this.inputElements()}
+            </div>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleSubmit}
+            >
+                Check
+            </Button>
+            {this.props.solution && <span className="button" onClick={this.toggleSolution}>Solution</span>}
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={this.newExercise}
+            >
+                New Exercise
+            </Button>
+            {/* {this.state.solutionVisible && <Solution steps={this.props.solution}/>} */}
+        </Paper>;
+    }
+}
+
+export default withStyles(styles)(Exercise);
